a47ce9352709b69959a898f8f16d7b6c33f79b18
Rajat Sahu <sahurjt@gmail.com>
2018-11-29 08:42:39 +0530

    Documented CsvColumn.java annnotation

--- src/com/sahurjt/objectcsv/annotations/CsvColumn.java
+++ src/com/sahurjt/objectcsv/annotations/CsvColumn.java
@@ -1,3 +1,8 @@
+/**
+ *  @author Rajat Sahu 
+ *  https://github.com/Rjtsahu/object-csv-java
+ *  ObjectCsv Java Library : Handy library to map CSV document to java model. 
+ * */
 package com.sahurjt.objectcsv.annotations;
 
 import java.lang.annotation.ElementType;
@@ -5,10 +10,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Anotation interface for Model which are having a CSV column,this anntation
+ * will be used in case the header row is not present in CSV.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface CsvColumn {
 
-	/// Map CSV value to particular column
+	/**
+	 *Column index of given field in CSV data.
+	 **/
 	public int coloumnIndex();
 }
